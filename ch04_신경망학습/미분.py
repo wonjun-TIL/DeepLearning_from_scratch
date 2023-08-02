a002c135e27fb9fb1aab1ba0fd1c7dc9886e8b70
Gardenjun <jeongwonjun0718@gmail.com>
2023-08-02 16:29:32 +0900

    fix : 문제점 두개 해결

--- ch04_신경망학습/미분.py
+++ ch04_신경망학습/미분.py
@@ -1,6 +1,12 @@
 
 
 # 나쁜 구현 예
-def numerical_diff(f, x):   ## 수치미분
+def numerical_diff_bad(f, x):   ## 수치미분
     h = 10e-50
     return (f(x + h) - f(x)) / h
+
+
+# 좋은 구현 예
+def numerical_diff(f, x):
+    h = 1e-4  # 0.0001
+    return (f(x + h) - f(x - h)) / (2 * h)  ## 중앙차분
