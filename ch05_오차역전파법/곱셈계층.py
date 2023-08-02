b1c78b0015e8d0f090aabfbd57ecb0f790c8aea5
Gardenjun <jeongwonjun0718@gmail.com>
2023-08-02 21:30:07 +0900

    feat : MulLayer 구현

---
+++ ch05_오차역전파법/곱셈계층.py
@@ -0,0 +1,19 @@
+
+class MulLayer:
+    def __init__(self):
+        self.x = None
+        self.y = None
+
+    def forward(self, x, y):
+        self.x = x
+        self.y = y
+        out = x * y
+
+        return out
+    
+    def backward(self, dout):
+        dx = dout * self.y # x와 y를 바꾼다.
+        dy = dout * self.x
+
+        return dx, dy
+    
