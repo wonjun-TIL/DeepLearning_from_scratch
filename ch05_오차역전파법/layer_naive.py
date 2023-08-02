54e4849f72797bfcc06e274836d8d7b55bedf72c
Gardenjun <jeongwonjun0718@gmail.com>
2023-08-02 21:49:00 +0900

    fix : 덧셈계층 구현

--- ch05_오차역전파법/layer_naive.py
+++ ch05_오차역전파법/layer_naive.py
@@ -17,3 +17,17 @@ class MulLayer:
 
         return dx, dy
     
+
+class AddLayer:
+    def __init__(self):
+        pass
+
+    def forward(self, x, y):
+        out = x + y
+
+        return out
+    
+    def backward(self, dout):
+        dx = dout * 1
+        dy = dout * 1
+        return dx, dy
