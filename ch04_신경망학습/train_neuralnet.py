254d69a1cb064a7df5c706fc323b8eb37679bca5
Gardenjun <jeongwonjun0718@gmail.com>
2023-08-02 20:18:39 +0900

    fix : 에폭을 이용한 오버피팅 테스트

--- ch04_신경망학습/train_neuralnet.py
+++ ch04_신경망학습/train_neuralnet.py
@@ -4,13 +4,19 @@ from two_layer_net import TwoLayerNet
 
 (x_train, t_train), (x_test, t_test) = load_mnist(normalize=True, one_hot_label=True)
 
-train_loss_list = []
 
 # 하이퍼파라미터
 iters_num = 10000 # 반복 횟수
 train_size = x_train.shape[0] # 60000
 batch_size = 100 # 미니배치 크기
 learning_rate = 0.1
+
+train_loss_list = []
+train_acc_list = []
+test_acc_list = []
+
+# 1에폭당 반복 수
+iter_per_epoch = max(train_size / batch_size, 1) # 600
 
 network = TwoLayerNet(input_size=784, hidden_size=50, output_size=10)
 
@@ -31,4 +37,13 @@ for i in range(iters_num):
     # 학습 경과 기록
     loss = network.loss(x_batch, t_batch) # 손실함수 값 계산
     train_loss_list.append(loss) # 손실함수 값 기록
+
+    # 1에폭당 정확도 계산
+    if i % iter_per_epoch == 0:
+        train_acc = network.accuracy(x_train, t_train)
+        test_acc = network.accuarcy(x_test, t_test)
+        train_acc_list.append(train_acc)
+        test_acc_list.append(test_acc)
+        print("train acc, test acc | " + str(train_acc) + ", " + str(test_acc))
+        
 
